8ddc5ba1c7b5ab1d5fa60084e8525dc02cc6b050
HIENLTH 2023 <hyhien@gmail.com>
2024-06-15 08:47:51 +0700

    Demo Nhap thong tin

---
+++ BaiTapBuoi02/BaiTapBuoi02/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapBuoi02.Controllers
+{
+    public class DemoController : Controller
+    {
+        // /Demo/NhapThongTin
+        public IActionResult NhapThongTin()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult KetQua(string HoTen, int NamSinh)
+        {
+            ViewBag.HoTen = HoTen;
+            ViewBag.NamSinh = NamSinh;
+
+            return View();
+        }
+    }
+}
