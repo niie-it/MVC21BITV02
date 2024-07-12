d90d27f73513d97eed6b6d60f0a493d0b9a21ee3
HIENLTH 2023 <hyhien@gmail.com>
2024-07-12 12:11:36 +0700

    Demo Area

---
+++ DemoLayout/DemoLayout/Areas/Admin/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoLayout.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DemoLayout/DemoLayout/Program.cs
+++ DemoLayout/DemoLayout/Program.cs
@@ -21,6 +21,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
