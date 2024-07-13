49b3f6bff97f2eaa33675252be9a38d6d90557a4
HIENLTH 2023 <hyhien@gmail.com>
2024-07-13 08:53:14 +0700

    Create frontend template

---
+++ L02WebApp/L02WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02WebApp.Controllers
+{
+	public class ProductsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Detail()
+		{
+			return View();
+		}
+	}
+}
---
+++ L02WebApp/L02WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02WebApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
