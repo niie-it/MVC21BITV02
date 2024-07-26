742540b132933928b11e97abcbde74c7d435c217
HIENLTH 2023 <hyhien@gmail.com>
2024-07-26 12:09:24 +0700

    Query data

---
+++ Lab07/Lab07/Controllers/Products.cs
@@ -0,0 +1,35 @@
+﻿using Lab07.Data;
+using Lab07.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab07.Controllers
+{
+    public class Products : Controller
+    {
+        private readonly MvcNiieLabContext _context;
+
+        public Products(MvcNiieLabContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/Products/{supplier_name}")]
+        public IActionResult GetProductBySuppliers(string supplier_name)
+        {
+            var data = _context.Products
+                .Where(p => p.Supplier.Name == supplier_name)
+                .Select(p => new ProductBySupplierVM {
+                    ProductId = p.Id,
+                    ProductName = p.Name,
+                    UnitPrice = p.UnitPrice,
+                    Category = p.Category.Name
+                });
+            return View(data);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab07/Lab07/Models/ProductBySupplierVM.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab07.Models
+{
+    public class ProductBySupplierVM
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set;}
+        public double UnitPrice { get; set;}
+        public string Status
+        {
+            get
+            {
+                if (UnitPrice > 50) return "Cheap";
+                else return "Normal";
+            }
+        }
+    }
+}
